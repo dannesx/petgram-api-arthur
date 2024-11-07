d77deef58b695434fef968371c9c47839945a615
dannesx <daniel.viantunes@gmail.com>
2024-11-07 16:38:12 -0300

    aaa

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,5 +1,8 @@
 import { NextFunction, Request, Response } from "express"
 import prisma from "../config/prisma"
+import { HTTPError } from "../utils/HTTPError"
+import { User } from "@prisma/client"
+import bcrypt from "bcryptjs"
 
 const select = {
     id: true,
@@ -8,20 +11,47 @@ const select = {
     createdAt: true
 }
 
+export async function getUsers(req: Request, res: Response, next: NextFunction) {
+    try {
+        const users = await prisma.user.findMany({ select })
 
-export async function getUsers(req: Request, res: Response) {
-    const users = await prisma.user.findMany({ select })
-
-    res.json(users)
+        res.json(users)
+    } catch (error) {
+        next(error)
+    }
 }
 
-export function getUserByUsername(req: Request, res: Response) { }
+export async function getUserByUsername(req: Request, res: Response, next: NextFunction) { 
+    try {
+        const { username } = req.params
+
+        const user = await prisma.user.findUnique({ where: { username }})
+
+        if (!user){
+            next(new HTTPError("User not found", 404))
+        }
+
+        res.json(user)
+    } catch (error) {
+        next(error)
+    }
+}
 
 export async function createUser(req: Request, res: Response, next: NextFunction) {
     try {
         const { username, email, password } = req.body
 
-        const newUser = await prisma.user.create({ data: { username, email, password }, select })
+        if (!username || !email || !password) {
+            next(new HTTPError("Username, email and password are required", 400))
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const newUser = await prisma.user.create({ data: {
+            username,
+            email,
+            password: hashedPassword
+        }, select })
 
         res.status(201).json(newUser)
     } catch (error) {
@@ -29,6 +59,39 @@ export async function createUser(req: Request, res: Response, next: NextFunction
     }
 }
 
-export function updateUser(req: Request, res: Response) { }
+export async function updateUser(req: Request, res: Response, next: NextFunction) {
+    try {
+        const { id } = req.params
+        const { username, email, password } = req.body
 
-export function deleteUser(req: Request, res: Response) { }
+        if (!username && !email && !password) {
+            return next(new HTTPError("Operation denied. Please provide at least username, email or password", 400))
+        }
+
+        const updateData: Partial<User> = {}
+
+        if (username) updateData.username = username
+        if (email) updateData.email = email
+        if (password) updateData.password = await bcrypt.hash(password, 10)
+
+        const updatedUser = await prisma.user.update({
+            where: { id },
+            data: updateData,
+            select
+        })
+
+        res.json(updatedUser)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export async function deleteUser(req: Request, res: Response, next: NextFunction) {
+    try {
+        const { id } = req.params
+        await prisma.user.delete({ where: { id }})
+        res.status(204).send()
+    } catch (error) {
+        next(error)
+    }
+}
--- src/middlewares/ErrorHandler.ts
+++ src/middlewares/ErrorHandler.ts
@@ -1,4 +1,6 @@
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import { NextFunction, Request, Response } from "express";
+import { HTTPError } from "../utils/HTTPError";
 
 export default function ErrorHandler(
     error: Error,
@@ -9,7 +11,22 @@ export default function ErrorHandler(
     let message = error.message || "Something went wrong"
     let status = 500
 
-    console.log("[ERROR]", message)
+    if (error instanceof PrismaClientKnownRequestError) {
+        const { code, meta } = error
+
+        switch (code) {
+            case 'P2002':
+                message = `Failed to create a new ${meta?.modelName}. This ${meta?.target} already exists`
+                status = 400
+                break
+        }
+    }
+
+    if (error instanceof HTTPError){
+        status = error.status
+    }
+
+    console.log("[ERROR]", error)
 
     res.status(status).json({
         error: true,
